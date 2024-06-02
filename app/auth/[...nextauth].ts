b39d6ab6a32b7b96e042728a135de5ad5e59afd8
54yd <ice.drifted@gmail.com>
2024-06-02 15:16:47 +0900

    *Remove pnpm lock for clean&tidy *Fix TailwindCSS not recognized problem ANSWER - by https://github.com/tailwindlabs/tailwindcss/issues/2831 npm uninstall tailwindcss postcss autoprefixer npm install tailwindcss@latest postcss@latest autoprefixer@latest
    npx tailwindcss init -p
    
    npm uninstall tailwindcss postcss autoprefixer
    npm install tailwindcss@npm:@tailwindcss/postcss7-compat postcss@^7 autoprefixer@^9

---
+++ app/auth/[...nextauth].ts
@@ -0,0 +1,21 @@
+
+import NextAuth from 'next-auth';
+import GitHubProvider from 'next-auth/providers/github';
+
+export const authOptions = {
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID!,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+    }),
+  ],
+  callbacks: {
+    async redirect({ url, baseUrl }) {
+      if (url.startsWith('/')) return `${baseUrl}${url}`;
+      else if (new URL(url).origin === baseUrl) return url;
+      return baseUrl;
+    },
+  },
+};
+
+export default NextAuth(authOptions);
