acb73ab424261f5a2f5d940ce605c7392e148c26
54yd <ice.drifted@gmail.com>
2024-10-28 11:31:34 +0900

    chore:rename footer placeholder

--- src/common/containers/OuterParts.tsx
+++ src/common/containers/OuterParts.tsx
@@ -1,24 +1,35 @@
-import React from'react';
-import { Layout, Menu } from 'antd';
-const { Header, Content, Footer } = Layout
+import React from "react";
+import { Layout, Menu } from "antd";
+const { Header, Content, Footer } = Layout;
 
-const AppHeader = (
-    { menuItems, onMenuClick }
-    :
-    { menuItems: string[]; onMenuClick: (page: string) => void }
-) => (
-    <Header style={{ backgroundColor: '#001529', padding: '0 50px' }}>
-        <div style={{ float: 'left', color: 'white', fontSize: '20px', fontWeight: 'bold' }}></div>
-        <Menu theme="dark" mode="horizontal" style={{ lineHeight: '64px' }}>
-            {menuItems.map((item, index) => (
-                <Menu.Item key={index} onClick={() => onMenuClick(item)}>{item}</Menu.Item>
-            ))}
-        </Menu>
-    </Header>
-)
+const AppHeader = ({
+  menuItems,
+  onMenuClick,
+}: {
+  menuItems: string[];
+  onMenuClick: (page: string) => void;
+}) => (
+  <Header style={{ backgroundColor: "#001529", padding: "0 50px" }}>
+    <div
+      style={{
+        float: "left",
+        color: "white",
+        fontSize: "20px",
+        fontWeight: "bold",
+      }}
+    ></div>
+    <Menu theme="dark" mode="horizontal" style={{ lineHeight: "64px" }}>
+      {menuItems.map((item, index) => (
+        <Menu.Item key={index} onClick={() => onMenuClick(item)}>
+          {item}
+        </Menu.Item>
+      ))}
+    </Menu>
+  </Header>
+);
 
 const AppFooter = () => (
-    <Footer style={{ textAlign: 'center' }}>Ant Design ©2024 Created by Ant UED</Footer>
-)
+  <Footer style={{ textAlign: "center" }}>54yd - Straydrop411 2024</Footer>
+);
 
-export { AppHeader, AppFooter }
+export { AppHeader, AppFooter };
